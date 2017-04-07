03c694d58026078c3566a4f8b9f2beb2aec62338
Ross Crichton <rcrichton87@googlemail.com>
2017-04-07 13:12:00 +0100

    customers have a name

---
+++ customer.js
@@ -0,0 +1,5 @@
+var Customer = function(name){
+  this.name = name
+}
+
+module.exports = Customer
---
+++ specs/customer_spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert')
+var Customer = require('../customer.js')
+
+describe("Customer", function(){
+
+  var customer1
+  var customer2
+
+  beforeEach(function(){
+    customer1 = new Customer("Ross")
+    customer2 = new Customer("Mike")
+  })
+
+  it("should have a name", function(){
+    assert.strictEqual("Ross", customer1.name)
+  })
+
+})
